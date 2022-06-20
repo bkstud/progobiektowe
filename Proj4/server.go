0d1764b6d064805dfeccd99a4d73b187d12af98b
Bazyli Kot <ciobab97@gmail.com>
2022-06-20 22:58:55 +0200

    refactor code to actually implement proxy design pattern

--- Proj4/server.go
+++ Proj4/server.go
@@ -11,27 +11,45 @@ import (
 
 var URL = "http://webcode.me"
 
+type GetProxy struct {
+	baseUrl string
+}
+
+func (gp *GetProxy) Get(endpoint string) (int, string, error) {
+	var url = gp.baseUrl + "/" + endpoint
+	resp, err := http.Get(url)
+	if err != nil {
+		return 0, "", err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+		return 0, "", err
+	}
+
+	return resp.StatusCode, string(body), nil
+}
+
 func main() {
 	e := echo.New()
+	gp := GetProxy{URL}
+
 	e.GET("/*", func(ctx echo.Context) error {
 		var endpoint string = ctx.ParamValues()[0]
-		var url = URL + "/" + endpoint
-		fmt.Println("Forwarding GET to URL: ", url)
-		resp, err := http.Get(url)
-		if err != nil {
-			return err
-		}
 
-		defer resp.Body.Close()
+		fmt.Println("Forwarding GET endpoint to proxy:", endpoint)
 
-		body, err := ioutil.ReadAll(resp.Body)
+		status, sb, err := gp.Get(endpoint)
 
 		if err != nil {
-			log.Fatal(err)
-			return err
+			panic(fmt.Errorf("Get proxy failed with error %v", err))
 		}
 
-		return ctx.String(resp.StatusCode, string(body))
+		return ctx.String(status, sb)
 	})
 
 	e.Logger.Fatal(e.Start(":1323"))
